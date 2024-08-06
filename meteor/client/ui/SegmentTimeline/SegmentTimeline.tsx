c21da97148ffec55169b66635dca55633c4f7ebf
olzzon <github@olzzon.dk>
2024-08-06 06:52:50 +0200

    WIP: UI indicators for userEditStates

--- meteor/client/ui/SegmentTimeline/SegmentTimeline.tsx
+++ meteor/client/ui/SegmentTimeline/SegmentTimeline.tsx
@@ -1104,6 +1104,27 @@ export class SegmentTimelineClass extends React.Component<Translated<WithTiming<
 							))}
 						</div>
 					)}
+					<div className="segment-timeline__title__user-edit-states">
+						{this.props.segment.userEditStates &&
+							Object.keys(this.props.segment.userEditStates).map((key) =>
+								this.props.segment.userEditStates?.[key] ? (
+									<div key={key}>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 16 16"
+											fill="none"
+											stroke="currentColor"
+											strokeWidth="1.5"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										>
+											<rect x="3" y="7" width="10" height="7" rx="1" ry="1" />
+											<path d="M5 7V5a3 3 0 0 1 6 0v2" />
+										</svg>
+									</div>
+								) : null
+							)}
+					</div>
 				</ContextMenuTrigger>
 				<div className="segment-timeline__duration" tabIndex={0}>
 					{this.props.playlist &&
