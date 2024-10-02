fd08c0ad962431b475f415fc98f6868a1f12218d
Mint de Wit <mint@superfly.tv>
2024-10-02 08:51:53 +0000

    fix(ui): autonext timing

--- packages/webui/src/client/lib/rundownTiming.ts
+++ packages/webui/src/client/lib/rundownTiming.ts
@@ -154,7 +154,11 @@ export class RundownTimingCalculator {
 				const partsSegment = segmentsMap.get(partInstance.segmentId)
 				const segmentBudget = partsSegment?.segmentTiming?.budgetDuration
 				const segmentUsesBudget = segmentBudget !== undefined
-				const lastStartedPlayback = partInstance.timings?.plannedStartedPlayback
+				// note: lastStartedPlayback that lies in the future means it hasn't started yet (like from autonext)
+				const lastStartedPlayback =
+					(partInstance.timings?.plannedStartedPlayback ?? 0) <= now
+						? partInstance.timings?.plannedStartedPlayback
+						: undefined
 
 				if (partInstance.segmentId !== lastSegmentId) {
 					this.untimedSegments.add(partInstance.segmentId)
