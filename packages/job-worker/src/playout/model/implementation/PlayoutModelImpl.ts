6df166801055d1e0163930170153d6ac8554e6fa
Mint de Wit <mint@superfly.tv>
2024-10-07 12:05:20 +0000

    fix: reset segments when added to an active loop

--- packages/job-worker/src/playout/model/implementation/PlayoutModelImpl.ts
+++ packages/job-worker/src/playout/model/implementation/PlayoutModelImpl.ts
@@ -792,6 +792,10 @@ export class PlayoutModelImpl extends PlayoutModelReadonlyImpl implements Playou
 		this.#playlistHasChanged = true
 	}
 
+	getSegmentsBetweenQuickLoopMarker(start: QuickLoopMarker, end: QuickLoopMarker): SegmentId[] {
+		return this.quickLoopService.getSegmentsBetweenMarkers(start, end)
+	}
+
 	/** Lifecycle */
 
 	/** @deprecated */
