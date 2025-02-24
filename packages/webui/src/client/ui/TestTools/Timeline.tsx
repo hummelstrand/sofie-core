8a8adc004f614f720b48d0e5203eb919efa0a85c
Julian Waller <git@julusian.co.uk>
2025-02-24 10:42:43 +0000

    feat: add ingest statuses debug page

--- packages/webui/src/client/ui/TestTools/Timeline.tsx
+++ packages/webui/src/client/ui/TestTools/Timeline.tsx
@@ -20,13 +20,8 @@ import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import Classnames from 'classnames'
-import { createSyncPeripheralDeviceCustomPublicationMongoCollection } from '../../collections/lib'
 import { StudioId } from '@sofie-automation/corelib/dist/dataModel/Ids'
-import { PeripheralDevicePubSubCollectionsNames } from '@sofie-automation/shared-lib/dist/pubsub/peripheralDevice'
-
-export const StudioTimeline = createSyncPeripheralDeviceCustomPublicationMongoCollection(
-	PeripheralDevicePubSubCollectionsNames.studioTimeline
-)
+import { StudioTimeline } from './collections'
 
 interface TimelineViewRouteParams {
 	studioId: string | undefined
